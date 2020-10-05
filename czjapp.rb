5483417ff0944ea2fc5bfd70111c67a639f7cae8
Adam Rambousek <rambousek@gmail.com>
2020-10-05 17:48:53 +0200

    Move lib file

--- czjapp.rb
+++ czjapp.rb
@@ -8,7 +8,7 @@ require 'open-uri'
 require 'i18n'
 require 'i18n/backend/fallbacks'
 
-require_relative 'lib/dict/czj'
+require_relative 'lib/czj'
 
 class CzjApp < Sinatra::Base
   $mongo = Mongo::Client.new([ '192.168.0.156:27017' ], :database => 'dictio')
