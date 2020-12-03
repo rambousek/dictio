b21f24a00d1d7dfa5ff749675bc09746a32749fd
Adam Rambousek <rambousek@gmail.com>
2020-12-03 14:06:53 +0100

    Add Help

--- czjapp.rb
+++ czjapp.rb
@@ -85,6 +85,16 @@ class CzjApp < Sinatra::Base
     slim page.to_sym
   end
 
+  get '/help' do
+    @dict_info = dict_info
+    @search_params = {}
+    @target = 'czj'
+    @dictcode = 'cs'
+    @selected_page = 'help'
+    page = 'help-'+I18n.locale.to_s
+    slim page.to_sym
+  end
+
   get '/contact' do
     @dict_info = dict_info
     @search_params = {}
