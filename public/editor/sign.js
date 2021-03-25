58789b6dcb469ccd6b5c77786b9c800c2b78a786
jfikejs <75360551+jfikejs@users.noreply.github.com>
2021-03-25 12:12:00 +0100

    Update sign.js

--- public/editor/sign.js
+++ public/editor/sign.js
@@ -3674,6 +3674,7 @@ function create_vyznam(entryid, add_copy, meaning_id) {
     },
     id: name,
     name: 'vyznam',
+    style: {dashed 2px #1c2641},
     frame: true,
     items: [{
       xtype: 'container',
