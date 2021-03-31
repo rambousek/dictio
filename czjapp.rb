b20a45dc5ff2c6f33fbd0242b4c2365204132740
Adam Rambousek <rambousek@gmail.com>
2021-03-31 17:28:59 +0200

    Enable logs

--- czjapp.rb
+++ czjapp.rb
@@ -22,6 +22,7 @@ class CzjApp < Sinatra::Base
     set :bind, '0.0.0.0'
     set :server, :puma
     set :strict_paths, false
+    set :logging, true
     I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
     I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
     I18n.backend.load_translations
