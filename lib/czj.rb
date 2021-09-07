46fb47e7a518ad4620d2ee1f523c1ef7a8c14ea6
Adam Rambousek <rambousek@gmail.com>
2021-09-07 14:54:28 +0200

    Search, by relation

--- lib/czj.rb
+++ lib/czj.rb
@@ -644,44 +644,23 @@ class CZJDict < Object
         else
           search_in = 'cs'
           search_in = @dict_info[dictcode]['search_in'] unless @dict_info[dictcode]['search_in'].nil?
-          csl = [search, search_orig]
-          if search != ''
-            search_cond_text = []
-            search_cond_text << {'lemma.title': search}
-            search_cond_text << {'lemma.title_var': search}
-            search_cond_text << {'lemma.title_dia': search}
-            search_cond_text << {'lemma.gram.form._text': search}
-            search_cond_text << {'lemma.title': {'$regex': /^#{search}/i}}
-            search_cond_text << {'lemma.title': {'$regex': /(^| )#{search}/i}}
-            $mongo['entries'].find({'dict'=>search_in, '$or'=>search_cond_text}, {'projection'=>{'meanings.id'=>1, '_id'=>0}}).each{|re|
-              unless re['meanings'].nil?
-                re['meanings'].each{|rl| 
-                  csl << rl['id']
-                }
-              end
-            }
+          csl = [search]
+          search_cond = {'source_dict': search_in, 'entry_text': {'$regex': /(^| )#{search}/i}, 'target': dictcode}
+          $mongo['relation'].find(search_cond).each{|rl|
+            csl << rl['target_id']
+          }
+          search_cond = {'source_dict': dictcode, 'target': search_in, 'source_id': {'$in': csl}}
+          collate = {:collation => {'locale' => 'cs', 'numericOrdering'=>true}, :sort => {'sort_key' => -1}}
+          if not $is_edit and not $is_admin
+            search_cond['status'] = 'published'
           end
-          search_cond = {'dict'=>dictcode}
-          search_cond_title = {'$or'=>[
-            {'meanings.relation'=>{'$elemMatch'=>{'target'=>search_in,'meaning_id'=>{'$in'=>csl}}}},
-            {'meanings.relation'=>{'$elemMatch'=>{'target'=>search_in,'meaning_id'=>{'$regex'=>/^#{search}$/i}}}}
-          ]}
-          if search != '' and more_params['slovni_druh'].to_s != ''
-            search_cond[:$and] = [search_cond_title,{'lemma.grammar_note.@slovni_druh'=> more_params['slovni_druh'].to_s}]
-          end
-          if search == '' and more_params['slovni_druh'].to_s != ''
-            search_cond['lemma.grammar_note.@slovni_druh'] = more_params['slovni_druh'].to_s
-          end
-          if search != '' and more_params['slovni_druh'].to_s == ''
-            search_cond[:$or] = search_cond_title['$or']
-          end
-          $stdout.puts search_cond
-          cursor = $mongo['entries'].find(search_cond, {:sort => {'sort_key' => -1}})
+          $stderr.puts search_cond
+          cursor = $mongo['relation'].find(search_cond, collate)
           resultcount = cursor.count_documents
           cursor = cursor.skip(start)
           cursor = cursor.limit(limit) if limit.to_i > 0
-          cursor.each{|e|
-            res << full_entry(e, false)
+          cursor.each{|entry|
+            res << full_entry(getone(entry['source_dict'], entry['source_id']), false)
           }
         end
       end
