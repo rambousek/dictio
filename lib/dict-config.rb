9e5570465e49b0f27a504941a9e09d5a7fa7e181
Adam Rambousek <rambousek@gmail.com>
2022-03-18 08:37:59 +0100

    add uk and uzm language

--- lib/dict-config.rb
+++ lib/dict-config.rb
@@ -3,9 +3,11 @@ $dict_info = {
     'en' => {'type' => 'write', 'label'=>'English', 'target'=>'is', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'part', 'adj', 'adv', 'prep', 'int']},
     'sj' => {'type' => 'write', 'label'=>'Slovak', 'target'=>'spj', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'part', 'adj', 'adv', 'prep', 'int']},
     'de' => {'type' => 'write', 'label'=>'German', 'target'=>'ogs', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'part', 'adj', 'adv', 'prep', 'int']},
+    'uk' => {'type' => 'write', 'label'=>'Ukrainian', 'target'=>'uzm', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'part', 'adj', 'adv', 'prep', 'int']},
     'czj' => {'type' => 'sign', 'label'=>'ČZJ', 'search_in'=>'cs', 'target'=>'cs', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
     'spj' => {'type' => 'sign', 'label'=>'SPJ', 'search_in'=>'sj', 'target'=>'sj', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
     'asl' => {'type' => 'sign', 'label'=>'ASL', 'search_in'=>'en', 'target'=>'en', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
     'is' => {'type' => 'sign', 'label'=>'IS', 'search_in'=>'en', 'target'=>'en', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
     'ogs' => {'type' => 'sign', 'label'=>'OGS', 'search_in'=>'de', 'target'=>'de', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
+    'uzm' => {'type' => 'sign', 'label'=>'UZM', 'search_in'=>'uk', 'target'=>'uk', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'part', 'taz', 'kat', 'klf', 'spc']},
   }
