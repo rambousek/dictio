b3da174d6226e80e36829ef7c02829fb0b48a249
jfikejs <fikejs@ics.muni.cz>
2024-10-15 14:53:09 +0200

    Revert "videobox hbox width test"
    This reverts commit 9092400af2311670f17b44c84150c6ee713c6c90.

--- public/editor/sign.js
+++ public/editor/sign.js
@@ -2760,7 +2760,6 @@ function create_text_video(idstart, entryid, label, show_copy, video_type) {
   var text = Ext.create('Ext.container.Container', {
         layout: {
           type: 'hbox'
-          width: 566,
         },
         cls: 'video-text',
         items: [{
