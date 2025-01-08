fc0122504a7f127c375b8ba51ade5825b82a0cd1
jfikejs <fikejs@ics.muni.cz>
2025-01-08 14:41:16 +0100

    repeated search_translate until found

--- czjapp.rb
+++ czjapp.rb
@@ -379,12 +379,39 @@ class CzjApp < Sinatra::Base
       @search = params['search']
       @input_type = params['type']
       @dictcode = code
-      @result = dict.translate2(code, params['target'], params['search'].to_s.strip, params['type'].to_s, params['start'].to_i, params['limit'].to_i)
-      if @result['count'] == 0
-        File.open("public/log/translate.csv", "a"){|f| f << [code, params['target'],  params['search'].to_s, Time.now.strftime("%Y-%m-%d %H:%M:%S")].join(";")+"\n"}
+
+      # Funkce pro překlad
+      def perform_search(search_term)
+        dict.translate2(code, params['target'], search_term.to_s.strip, params['type'].to_s, params['start'].to_i, params['limit'].to_i)
       end
-      slim :transresultlist, :layout=>false
-    end
+      
+      # První pokus o hledání
+      @result = perform_search(@search)
+      
+      # Pokud nejsou nalezeny výsledky, zalogovat a iterativně zkracovat
+      if @result['count'] == 0
+        # Zalogování původního hledaného výrazu
+        File.open("public/log/translate.csv", "a") do |f|
+          f << [code, params['target'], @search.to_s, Time.now.strftime("%Y-%m-%d %H:%M:%S")].join(";") + "\n"
+        end
+      
+        # Iterativní zkracování výrazu, dokud nejsou nalezeny výsledky nebo není výraz prázdný
+        while @result['count'] == 0 && @search.length > 1
+          @search = @search[0..-2] # Zkrátit o jeden znak od konce
+          @result = perform_search(@search)
+        end
+      end
+      
+      # Zobrazení výsledků
+      slim :transresultlist, layout: false
+      end
+
+    #  @result = dict.translate2(code, params['target'], params['search'].to_s.strip, params['type'].to_s, params['start'].to_i, params['limit'].to_i)
+    #  if @result['count'] == 0
+    #    File.open("public/log/translate.csv", "a"){|f| f << [code, params['target'],  params['search'].to_s, Time.now.strftime("%Y-%m-%d %H:%M:%S")].join(";")+"\n"}
+    #  end
+    #  slim :transresultlist, :layout=>false
+    # end
     get '/'+code+'/revcolloc/:id' do
       @entry = dict.get_revcolloc(params['id'], 'collocation')
       @type = 'collocation'
