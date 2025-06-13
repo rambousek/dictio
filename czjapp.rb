0f7f72ca9c261b5107a879332491aae0bd0016d3
Adam Rambousek <rambousek@gmail.com>
2025-06-13 17:15:01 +0200

    Export synonym,variant,sl.druh

--- czjapp.rb
+++ czjapp.rb
@@ -683,7 +683,7 @@ class CzjApp < Sinatra::Base
       content_type 'text/csv; charset=utf-8'
       attachment code+'report.csv'
       if $dict_info[code]['type'] == 'sign'
-        csv = ['ID;video čelní;video boční;překlady;překlady text;fsw']
+	csv = ['ID;video čelní;video boční;překlady;překlady text;fsw;synonyma;varianty;sl.druh;sl.druh2;sl.druh3']
         dict.get_report(params, @user_info)['entries'].each{|rep|
           ri = [rep['id']]
           ri << rep['lemma']['video_front'].to_s
@@ -719,6 +719,50 @@ class CzjApp < Sinatra::Base
             }
             ri << sws.join(',')
           end
+	  # synonyma
+	  syns = []
+	  if rep['meanings']
+	  rep['meanings'].each{|rm|
+            if rm['relation']
+		    rm['relation'].select{|rel| rel['type'] == 'synonym'}.each{|rr|
+			    syns << rr['meaning_id'].split('-')[0]
+		    }
+	    end
+	  }
+	  end
+	  ri << syns.join(',')
+	  # varianty
+	  vars = []
+	  if rep['lemma']['grammar_note'] and rep['lemma']['grammar_note'][0]['variant']
+		  rep['lemma']['grammar_note'][0]['variant'].each{|var|
+			  vars << var['_text'] if var['_text']
+		  }
+	  end
+	  if rep['lemma']['style_note'] and rep['lemma']['style_note'][0]['variant']
+		  rep['lemma']['style_note'][0]['variant'].each{|var|
+			  vars << var['_text'] if var['_text']
+		  }
+	  end
+	  ri << vars.join(',')
+	  # slovni druh
+	  pos = ''
+	  pos2 = ''
+	  pos3 = ''
+	  if rep['lemma']['grammar_note'] and rep['lemma']['grammar_note'][0] 
+		  if rep['lemma']['grammar_note'][0]['@slovni_druh']
+			  pos = rep['lemma']['grammar_note'][0]['@slovni_druh']
+		  end
+		  if rep['lemma']['grammar_note'][0]['@skupina']
+			  pos2 = rep['lemma']['grammar_note'][0]['@skupina']
+		  end
+		  if rep['lemma']['grammar_note'][0]['@skupina2']
+			  pos3 = rep['lemma']['grammar_note'][0]['@skupina2']
+		  end
+	  end
+	  ri << pos
+	  ri << pos2
+	  ri << pos3
+
           csv << ri.join(';')
         }
       else
