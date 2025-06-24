935b034ba90a4e7563d94f5bc4237f087b7a8c6b
Adam Rambousek <rambousek@gmail.com>
2025-06-24 11:16:57 +0200

    reformat dictionary config

--- lib/dict-config.rb
+++ lib/dict-config.rb
@@ -1,14 +1,16 @@
+pos_sign = %w[subst verb modif pron num konj par taz kat klf spc]
+pos_write = %w[subst verb pron num konj par adj adv prep int]
 $dict_info = {
-    'cs' => {'type' => 'write', 'label'=>'Czech', 'target'=>'czj', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'par', 'adj', 'adv', 'prep', 'int']},
-    'czj' => {'type' => 'sign', 'label'=>'ČZJ', 'search_in'=>'cs', 'target'=>'cs', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc', 'finger']},
-    'en' => {'type' => 'write', 'label'=>'English', 'target'=>'is', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'par', 'adj', 'adv', 'prep', 'int']},
-    'asl' => {'type' => 'sign', 'label'=>'ASL', 'search_in'=>'en', 'target'=>'en', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc']},
-    'is' => {'type' => 'sign', 'label'=>'IS', 'search_in'=>'en', 'target'=>'en', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc']},
-    'sj' => {'type' => 'write', 'label'=>'Slovak', 'target'=>'spj', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'par', 'adj', 'adv', 'prep', 'int']},
-    'spj' => {'type' => 'sign', 'label'=>'SPJ', 'search_in'=>'sj', 'target'=>'sj', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc']},
-    'de' => {'type' => 'write', 'label'=>'German', 'target'=>'ogs', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'par', 'adj', 'adv', 'prep', 'int']},
-    'ogs' => {'type' => 'sign', 'label'=>'OGS', 'search_in'=>'de', 'target'=>'de', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc']},
-    'uk' => {'type' => 'write', 'label'=>'Ukrainian', 'target'=>'uzm', 'pos'=>['subst', 'verb', 'pron', 'num', 'konj', 'par', 'adj', 'adv', 'prep', 'int']},
-    'uzm' => {'type' => 'sign', 'label'=>'UZM', 'search_in'=>'uk', 'target'=>'uk', 'pos'=>['subst', 'verb', 'modif', 'pron', 'num', 'konj', 'par', 'taz', 'kat', 'klf', 'spc']},
+    'cs' => {'type' => 'write', 'label'=>'Czech', 'target'=>'czj', 'pos'=>pos_write},
+    'czj' => {'type' => 'sign', 'label'=>'ČZJ', 'search_in'=>'cs', 'target'=>'cs', 'pos'=>pos_sign+['finger']},
+    'en' => {'type' => 'write', 'label'=>'English', 'target'=>'is', 'pos'=>pos_write},
+    'asl' => {'type' => 'sign', 'label'=>'ASL', 'search_in'=>'en', 'target'=>'en', 'pos'=>pos_sign},
+    'is' => {'type' => 'sign', 'label'=>'IS', 'search_in'=>'en', 'target'=>'en', 'pos'=>pos_sign},
+    'sj' => {'type' => 'write', 'label'=>'Slovak', 'target'=>'spj', 'pos'=>pos_write},
+    'spj' => {'type' => 'sign', 'label'=>'SPJ', 'search_in'=>'sj', 'target'=>'sj', 'pos'=>pos_sign},
+    'de' => {'type' => 'write', 'label'=>'German', 'target'=>'ogs', 'pos'=>pos_write},
+    'ogs' => {'type' => 'sign', 'label'=>'OGS', 'search_in'=>'de', 'target'=>'de', 'pos'=>pos_sign},
+    'uk' => {'type' => 'write', 'label'=>'Ukrainian', 'target'=>'uzm', 'pos'=>pos_write},
+    'uzm' => {'type' => 'sign', 'label'=>'UZM', 'search_in'=>'uk', 'target'=>'uk', 'pos'=>pos_sign},
   }
 $fsw_style = '-CG_white_'
