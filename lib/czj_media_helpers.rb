24469a4a87fb9ad54ad4d9e681288977716dafcf
Adam Rambousek <rambousek@gmail.com>
2026-06-19 13:05:40 +0200

    fixes

--- lib/czj_media_helpers.rb
+++ lib/czj_media_helpers.rb
@@ -10,32 +10,32 @@ module CzjMediaHelpers
           media_info['main_for_entry'] = @sw.get_sw(entries.first)
         end
       end
-      return media_info
+      media_info
     else
-      return {}
+      {}
     end
   end
 
   def get_media_location(media_id, dict)
     media = $mongo['media'].find({'location': media_id, 'dict': dict})
     if media.first
-      return media.first
+      media.first
     else
-      return {}
+      {}
     end
   end
 
   def add_media(entry, main_only=false)
     entry['media'] = {}
-    if not main_only
+    unless main_only
       if entry['meanings']
-        entry['meanings'].each{|mean|
+        entry['meanings'].each { |mean|
           if mean['text'] and mean['text'].is_a?(Hash) and mean['text']['file']
             if mean['text']['file'].is_a?(Hash)
               entry['media'][mean['text']['file']['@media_id'].to_s] = get_media(mean['text']['file']['@media_id'].to_s, entry['dict'])
             end
             if mean['text']['file'].is_a?(Array)
-              mean['text']['file'].each{|mv|
+              mean['text']['file'].each { |mv|
                 if mv['@media_id']
                   entry['media'][mv['@media_id'].to_s] = get_media(mv['@media_id'].to_s, entry['dict'])
                 end
@@ -43,13 +43,13 @@ module CzjMediaHelpers
             end
           end
           if mean['usages']
-            mean['usages'].each{|usg|
-              if usg['text'] and usg['text']['file'] 
+            mean['usages'].each { |usg|
+              if usg['text'] and usg['text']['file']
                 if usg['text']['file'].is_a?(Hash)
                   entry['media'][usg['text']['file']['@media_id']] = get_media(usg['text']['file']['@media_id'], entry['dict'])
                 end
                 if usg['text']['file'].is_a?(Array)
-                  usg['text']['file'].each{|fm|
+                  usg['text']['file'].each { |fm|
                     entry['media'][fm['@media_id']] = get_media(fm['@media_id'], entry['dict'])
                   }
                 end
@@ -59,28 +59,28 @@ module CzjMediaHelpers
         }
       end
       if entry['lemma'] and entry['lemma']['grammar_note']
-        entry['lemma']['grammar_note'].each{|gn|
+        entry['lemma']['grammar_note'].each { |gn|
           if gn['variant']
-            gn['variant'].each{|gv|
+            gn['variant'].each { |gv|
               entry['media'][gv['_text']] = get_media(gv['_text'], entry['dict']) if gv['_text'] != ''
             }
           end
           if gn['_text'] and gn['_text'] =~ /media_id/
-            gn['_text'].scan(/\[media_id=([0-9]+)\]/).each{|gm|
+            gn['_text'].scan(/\[media_id=([0-9]+)\]/).each { |gm|
               entry['media'][gm[0]] = get_media(gm[0], entry['dict'])
             }
           end
         }
       end
       if entry['lemma'] and entry['lemma']['style_note']
-        entry['lemma']['style_note'].each{|gn|
+        entry['lemma']['style_note'].each { |gn|
           if gn['variant']
-            gn['variant'].each{|gv|
+            gn['variant'].each { |gv|
               entry['media'][gv['_text']] = get_media(gv['_text'], entry['dict']) if gv['_text'] != ''
             }
           end
           if gn['_text'] and gn['_text'] =~ /media_id/
-            gn['_text'].scan(/\[media_id=([0-9]+)\]/).each{|gm|
+            gn['_text'].scan(/\[media_id=([0-9]+)\]/).each { |gm|
               entry['media'][gm[0]] = get_media(gm[0], entry['dict'])
             }
           end
@@ -95,7 +95,7 @@ module CzjMediaHelpers
         entry['media']['video_side'] = get_media_location(entry['lemma']['video_side'].to_s, entry['dict'])
       end
     end
-    return entry
+    entry
   end
 
   def get_usage_target(entry, usage_id)
