99c99148113eb18b10973e9173afd836301ad502
Adam Rambousek <rambousek@gmail.com>
2026-07-18 12:54:47 +0200

    split static page routes into routes/pages.rb

---
+++ routes/pages.rb
@@ -0,0 +1,58 @@
+class CzjApp < Sinatra::Base
+  get '/' do
+    @dict_info = $dict_info
+    @search_params = {}
+    @request = request
+    stat = $mongo['entryStat'].find({}, :sort=>{'dateField'=>-1}).first
+    @count_entry = stat['entries'][0]['count']
+    @count_rels = ((stat['rel'][0]['count'].to_i+stat['usgrel'][0]['count'].to_i)/2).round
+    @params = params
+    @cite_attr = CzjWebHelper.get_cite_attr('page', request.path_info, 'index')
+    @cite_text = CzjWebHelper.build_cite(@cite_attr)
+    slim :home
+  end
+
+  get '/about' do
+    @dict_info = $dict_info
+    @search_params = {}
+    @request = request
+    @selected_page = 'about'
+    page = 'about-'+I18n.locale.to_s
+    @cite_attr = CzjWebHelper.get_cite_attr('page', request.path_info, @selected_page)
+    @cite_text = CzjWebHelper.build_cite(@cite_attr)
+    slim page.to_sym
+  end
+
+  get '/help' do
+    @dict_info = $dict_info
+    @search_params = {}
+    @request = request
+    @selected_page = 'help'
+    page = 'help-'+I18n.locale.to_s
+    @cite_attr = CzjWebHelper.get_cite_attr('page', request.path_info, @selected_page)
+    @cite_text = CzjWebHelper.build_cite(@cite_attr)
+    slim page.to_sym
+  end
+
+  get '/helpsign' do
+    @dict_info = $dict_info
+    @search_params = {}
+    @request = request
+    @selected_page = 'help'
+    page = 'helpsign-'+I18n.locale.to_s
+    @cite_attr = CzjWebHelper.get_cite_attr('page', request.path_info, @selected_page)
+    @cite_text = CzjWebHelper.build_cite(@cite_attr)
+    slim page.to_sym
+  end
+
+  get '/contact' do
+    @dict_info = $dict_info
+    @search_params = {}
+    @request = request
+    @selected_page = 'contact'
+    page = 'contact-'+I18n.locale.to_s
+    @cite_attr = CzjWebHelper.get_cite_attr('page', request.path_info, @selected_page)
+    @cite_text = CzjWebHelper.build_cite(@cite_attr)
+    slim page.to_sym
+  end
+end
